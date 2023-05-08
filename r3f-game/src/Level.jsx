71ad0085837618d7130d02ce309d51c5fa7c86c9
KenTra222 <Kentraviousc@gmail.com>
2023-05-08 18:42:04 +0000

    blockLimbo added to tlevel

--- r3f-game/src/Level.jsx
+++ r3f-game/src/Level.jsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import {useRef } from 'react'
+import {useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { RigidBody } from '@react-three/rapier'
 
@@ -31,10 +31,19 @@ const BlockStart = ({ position = [ 0, 0, 0 ]}) =>
 function BlockSpinner({ position = [ 0, 0, 0 ] })
 {
   const obstacle = useRef()
+  const [speed] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1 ))
+  
 
   useFrame((state) =>
     {
+      // gets the time
         const time = state.clock.getElapsedTime()
+      // creates a quaternion so that it spins
+        const rotation = new THREE.Quaternion()
+        //creates a euler
+        rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+      // attaches it to the obstacle
+      obstacle.current.setNextKinematicRotation(rotation)
 
     })
 
@@ -42,9 +51,45 @@ function BlockSpinner({ position = [ 0, 0, 0 ] })
       {/* floor */}
         <mesh geometry={ boxGeometry } material={ floor2Material } position={ [ 0, - 0.1, 0 ] } scale={ [ 4, 0.2, 4 ] } receiveShadow />
         
-        {/* obstacle */}
+        {/*  twister obstacle */}
         <RigidBody 
           ref={ obstacle } 
+          type="kinematicPosition" 
+          position={[0, 0.3, 0]} 
+          restitution={ 0.2 } 
+          friction={ 0 }>
+
+         <mesh geometry={ boxGeometry } material={ obstacleMaterial } scale={ [ 3.5, 0.3, 0.3 ] } castShadow receiveShadow />
+        </RigidBody>
+    </group>
+}
+
+//limbo animation
+function BlockLimbo({ position = [ 0, 0, 0 ] })
+{
+  const obstacle = useRef()
+  //offsets the obstacel
+  const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
+  
+
+  useFrame((state) =>
+    {
+      // gets the time
+        const time = state.clock.getElapsedTime()
+        //makes obstacle go up and down
+        const y = Math.sin(time * timeOffset) + 1.4
+        obstacle.current.setNextKinematicTranslation({ x:position[0], y: position[1] + y , z: position[2]})
+
+    })
+
+    return <group position={ position }>
+      {/* floor */}
+        <mesh geometry={ boxGeometry } material={ floor2Material } position={ [ 0, - 0.1, 0 ] } scale={ [ 4, 0.2, 4 ] } receiveShadow />
+        
+        {/*  twister obstacle */}
+        <RigidBody 
+          ref={ obstacle } 
+          //adds custom physic type for useframe
           type="kinematicPosition" 
           position={[0, 0.3, 0]} 
           restitution={ 0.2 } 
@@ -65,8 +110,9 @@ export default function  Level() {
        
         <RigidBody type='fixed'>
 
-        <BlockStart position={[0,0,4]} />
-        <BlockSpinner position={[0,0,0]} />
+        <BlockStart position={[0,0,8]} />
+        <BlockSpinner position={[0,0,4]} />
+        <BlockLimbo/>
         </RigidBody>
 
 </>
