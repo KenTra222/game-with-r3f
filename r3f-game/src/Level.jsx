494e9ce2c396291644e5239b1a3635ccc97ef173
KenTra222 <Kentraviousc@gmail.com>
2023-05-09 20:57:15 +0000

    updated Levels and added walls

--- r3f-game/src/Level.jsx
+++ r3f-game/src/Level.jsx
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
-import {useRef, useState } from 'react'
+import {useRef, useState, useMemo } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { RigidBody } from '@react-three/rapier'
+import { useGLTF } from '@react-three/drei'
 
 THREE.ColorManagement.legacyMode = false
 
@@ -15,7 +16,7 @@ const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey"})
 
 
 // floor
-const BlockStart = ({ position = [ 0, 0, 0 ]}) =>
+export const BlockStart = ({ position = [ 0, 0, 0 ]}) =>
 {
 
     return <group position={ position }>
@@ -28,7 +29,7 @@ const BlockStart = ({ position = [ 0, 0, 0 ]}) =>
     </group>
 }
 
-function BlockSpinner({ position = [ 0, 0, 0 ] })
+export   function BlockSpinner({ position = [ 0, 0, 0 ] })
 {
   const obstacle = useRef()
   const [speed] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1 ))
@@ -65,7 +66,7 @@ function BlockSpinner({ position = [ 0, 0, 0 ] })
 }
 
 //limbo animation
-function BlockLimbo({ position = [ 0, 0, 0 ] })
+export function BlockLimbo({ position = [ 0, 0, 0 ] })
 {
   const obstacle = useRef()
   //offsets the obstacel
@@ -100,21 +101,111 @@ function BlockLimbo({ position = [ 0, 0, 0 ] })
     </group>
 }
 
+//axe animation
+export function BlockAxe({ position = [ 0, 0, 0 ] })
+{
+  const obstacle = useRef()
+  //offsets the obstacel
+  const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
+  
 
+  useFrame((state) =>
+    {
+      // gets the time
+        const time = state.clock.getElapsedTime()
+        //makes obstacle side to side
+        const x = Math.sin(time * timeOffset) * 1.25
+        obstacle.current.setNextKinematicTranslation({ x:position[0] + x, y: position[1]  + 0.75, z: position[2]})
 
-export default function  Level() {
+    })
+
+    return <group position={ position }>
+      {/* floor */}
+        <mesh geometry={ boxGeometry } material={ floor2Material } position={ [ 0, - 0.1, 0 ] } scale={ [ 4, 0.2, 4 ] } receiveShadow />
+        
+        {/*  twister obstacle */}
+        <RigidBody 
+          ref={ obstacle } 
+          //adds custom physic type for useframe
+          type="kinematicPosition" 
+          position={[0, 0.3, 0]} 
+          restitution={ 0.2 } 
+          friction={ 0 }>
+
+    <mesh geometry={ boxGeometry } material={ obstacleMaterial } scale={ [ 1.5, 1.5, 0.3 ] } castShadow receiveShadow />
+    </RigidBody>
+    </group>
+}
+
+export const BlockEnd = ({ position = [ 0, 0, 0 ]}) =>
+{
+  //imports model
+  const hamburger = useGLTF('./hamburger.glb')
+
+  //casts a shadow
+  hamburger.scene.children.forEach((mesh)=>{
+    mesh.castShadow=true
+  })
+
+    return <group position={ position }>
+     <mesh geometry={ boxGeometry } material={ floorMaterial } position={ [ 0, 0, 0 ] } scale={ [ 4, 0.2, 4 ] } receiveShadow />    
     
+    {/* goal */}
+     <RigidBody type="fixed" colliders='hull' position={[ 0, 0.2, 0]} restitution={ 0.2 } friction={ 0 }> 
+      <primitive object={hamburger.scene} scale={0.25}/>     
+     </RigidBody>
+    </group>
+}
+
+//Bounds
+
+ function Bounds({ length=1}){
+  return<>
+  {/* right wall */}
+    <mesh
+      position={ [ 2.15, .75, - (length * 2) + 2 ] }
+       geometry={boxGeometry} 
+       scale={[0.3, 1.5, 4 *length]}
+       castShadow/>
+
+  {/* left wall */}
+<mesh
+      position={ [ -2.15, .75, - (length * 2) + 2 ] }
+       geometry={boxGeometry} 
+       scale={[0.3, 1.5, 4 *length]}
+       />
+  </>
+}
+
+
+
+export function  Level({count = 5, types=[BlockAxe,BlockLimbo,BlockSpinner]}) {
+
+
+  // return a blocks array and set the dependencies to count and types
+  const blocks = useMemo(()=>{
+    const blocks = []
+
+    //loops through the types
+    for(let i = 0; i< count; i++){
+      //makes the types random
+      const type = types[Math.floor(Math.random() * types.length)]
+      //pushes results to the type
+       blocks.push(type)
+    }
+
+    return blocks
+  }, [count, types])
 
   return (
 <>
-       
-        <RigidBody type='fixed'>
-
-        <BlockStart position={[0,0,8]} />
-        <BlockSpinner position={[0,0,4]} />
-        <BlockLimbo/>
-        </RigidBody>
-
+       {/* level */}
+        <BlockStart position={ [ 0, 0, 0 ] } />
+        {/* maps out the traps */}
+        {blocks.map((Block, index) => <Block key={index} position={[ 0, 0, -( index + 1)*4]}/>)}
+        <BlockEnd position={[0,0, - (count +1) * 4]}/>
+        
+        <Bounds length={count+2}/>
 </>
 
   )
